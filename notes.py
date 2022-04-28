4545bafbf7c0207a7ef3671d2c010004a727b7de
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-28 12:50:20 -0400

    function with more than one input - notes

--- notes.py
+++ notes.py
@@ -1,16 +1,22 @@
 #DAY8: Functions with inputs
 
-#Simple Function
-def greet():
-    print("Hello Shaun")
-    print(f"How do you do Shaun?")
-    print("Isn't the weather nice today?")
-greet()
+# #Simple Function
+# def greet():
+#     print("Hello Shaun")
+#     print(f"How do you do Shaun?")
+#     print("Isn't the weather nice today?")
+# greet()
 
-#Functions with input
-def greet_with_name(name):
-    print(f"Hello {name} ")
-    print(f"How do you do {name}?")
+# #Functions with input
+# def greet_with_name(name):
+#     print(f"Hello {name} ")
+#     print(f"How do you do {name}?")
 
-greet_with_name("George")
+# greet_with_name("George")
 
+#Function with more than one input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Shaun", "Florida")
