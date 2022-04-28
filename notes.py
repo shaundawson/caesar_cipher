b912cb94dd0c606a70ebd659f21889ff835d3766
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-28 12:42:45 -0400

    function that allows for input -  notes

---
+++ notes.py
@@ -0,0 +1,16 @@
+#DAY8: Functions with inputs
+
+#Simple Function
+def greet():
+    print("Hello Shaun")
+    print(f"How do you do Shaun?")
+    print("Isn't the weather nice today?")
+greet()
+
+#Functions with input
+def greet_with_name(name):
+    print(f"Hello {name} ")
+    print(f"How do you do {name}?")
+
+greet_with_name("George")
+
