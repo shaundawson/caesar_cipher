84c560e548c0772aee099a0824d20852e5a0977d
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-28 14:14:47 -0400

    exercise - prime number checker

--- notes.py
+++ notes.py
@@ -27,16 +27,38 @@
 
 
 
-#EXERCISE: PAINT AREA CALCULATOR
-# You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
+# #EXERCISE: PAINT AREA CALCULATOR
+# # You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy. area = width * height
 
-import math
+# import math
 
-def paint_calc(height, width, cover):
-    number_of_cans = math.ceil((height * width) / cover)
-    print(f"You'll need {number_of_cans} cans of paint")
+# def paint_calc(height, width, cover):
+#     number_of_cans = math.ceil((height * width) / cover)
+#     print(f"You'll need {number_of_cans} cans of paint")
 
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
+#EXERCISE: PRIME NUMBER CHECKER
+#Prime numbers are numbers that can only be cleanly divided by themselves and 1.  Write a function that checks whether the number passed into it is a prime number or not.
+
+def prime_checker(number):
+    if number > 1:
+    # Iterate from 2 to n / 2
+        for i in range(2, int(number/2)+1): 
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (number % i) == 0:
+                print(f"{number} is not a prime number")
+                break
+        else:
+            print(f"{number} is a prime number")
+  
+    else:
+        print(f"{number} is not a prime number")
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
